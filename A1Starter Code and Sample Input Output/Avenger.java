00f19d0df7aa075c55327886aaec33cd394670ea
Sidhant-Kaushik <sidhant9101@gmail.com>
2020-09-27 22:31:49 -0600

    commented Avenger

--- A1Starter Code and Sample Input Output/Avenger.java
+++ A1Starter Code and Sample Input Output/Avenger.java
@@ -14,32 +14,51 @@ public class Avenger implements Comparable <Avenger> {
     public void setFreq() {
 		frequency = 1;
 	}
-
+    /*
+     * increaseFreq increases the frequency of Avenger by 1
+     */
     public void increaseFreq() {
         frequency++;
     }
-    
+    /*
+     * getFreq returns the frequency
+     */
     public int getFreq() {
     	return frequency;
     }
-    
+    /*
+     * getName returns the Hero name
+     */
     public String getName() {
     	return heroName;
     }
-    
+    /*
+     * get Alias returns the Hero's alias
+     */
     public String getAlias() {
     	return heroAlias;
     }
 	
 	@Override
+	/*
+	 * compareTo takes in one Avenger and compares it to another
+	 * @param Avenger needed to compare one to the other
+	 */
 	public int compareTo(Avenger other) {
 		return this.heroAlias.compareTo(other.heroAlias);
 	}
-
-    public boolean equals (Avenger other) {
+	
+	/*
+	 * equals takes in one avenger and compares it with the other avenger
+	 * @param Avenger
+	 */
+    public boolean equals(Avenger other) {
         return this.heroAlias.equals(other.heroAlias);
     }
     @Override
+    /*
+     * overriding toString to fit the format that was specified
+     */
     public String toString() {
     	String format = heroAlias + " aka " + heroName
     			+ " mentioned " + frequency + " time(s)";
