9c3ab59eb176c6893a301949710a4a534702c10d
Sidhant-Kaushik <sidhant9101@gmail.com>
2020-09-27 23:40:22 -0600

    Update A1.java

--- A1Starter Code and Sample Input Output/A1.java
+++ A1Starter Code and Sample Input Output/A1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * This program must read a input stream and keeps track of the 
  * frequency at which an avenger is mentioned either by name or alias.
  *
- * @author Maryam Elahi
+ * @author Maryam Elahi, Sidhant Kaushik, Erika Robles
  * @date Fall 2020
 */
 
